2f930db8a872a624522e07e9f67fbd7cc312b421
Harald Schilly <harald.schilly@gmail.com>
2012-10-27 16:02:29 +0200

    server: signing partial tx and qr encoding

--- server/test.py
+++ server/test.py
@@ -1,27 +1,52 @@
 #!/usr/bin/env python
-from json import dumps
 import bitcoinrpc
-from bitcoinrpc.exceptions import BitcoinException,InsufficientFunds
-from bitcoinrpc.config import read_config_file
+#from bitcoinrpc.exceptions import BitcoinException,InsufficientFunds
+#from bitcoinrpc.config import read_config_file
 
 def gen_2of3(conn, pub1, pub2, pub3):
   return conn.proxy.addmultisigaddress(2, [pub1, pub2, pub3])
 
-def check_rcv_2of3(conn, adr_2of3, value):
-  cur_block = conn.getblockcount()
-  while cur_block > 33840:
-    print cur_block
-    blkhash = conn.proxy.getblockhash(cur_block)
-    blk = conn.proxy.getblock(blkhash)
-    for tx in blk["tx"]:
+def check_rcv_2of3(conn, adr_2of3, value, searchdepth = 100):
+  def check(tx_list):
+    for tx in tx_list:
       raw = conn.proxy.getrawtransaction(tx)
       t = conn.proxy.decoderawtransaction(raw)
       for v in t["vout"]:
         for adr in v["scriptPubKey"].get("addresses", []):
           if adr_2of3 == adr and v["value"] >= value:
-            return True
+            return True, tx, v["n"]
+
+  proxy = conn.proxy
+
+  mempool = proxy.getrawmempool()
+  c = check(mempool)
+  if c and c[0]:
+    print "true in mempool"
+    return c
+
+  cur_block = conn.getblockcount()
+  limit_blocknr = cur_block - searchdepth
+  while cur_block > limit_blocknr:
+    print cur_block
+    blkhash = conn.proxy.getblockhash(cur_block)
+    blk = conn.proxy.getblock(blkhash)
+    c = check(blk["tx"])
+    if c and c[0]:
+      print "true in block %s" % cur_block
+      return c
     cur_block -= 1
   return False
+
+def gen_partial_tx(conn, target_addr, txid, voutid, amount):
+  proxy = conn.proxy
+  rawtx = proxy.createrawtransaction([{'txid' : txid, 'vout': voutid}], {target_addr : amount })
+  return rawtx
+
+def sign_rawtx(conn, partial_tx):
+  signed = conn.proxy.signrawtransaction(partial_tx)
+  decoded = conn.proxy.decoderawtransaction(signed['hex'])
+  assert decoded['vout'][0]['scriptPubKey']['reqSigs'] == 1
+  return signed['hex']
 
 def run():
   conn = bitcoinrpc.connect_to_local()
@@ -49,13 +74,37 @@ def run():
 
   print
   print "RECIEVED?"
-  print check_rcv_2of3(conn, multisig, 0.1)
+  check = check_rcv_2of3(conn, multisig, 0.1, searchdepth = 100)
+  print check
+  if not check[0]:
+    return
+
+  print
+  target_addr = "myKPhLdmfk6Ss8j6ugqCVwsC3bcUHpZCg5"
+  amount = 0.1
+  print "RELEASE %s BTC to %s" % (amount, target_addr)
+  _, txid, voutid = check
+  partial_tx = gen_partial_tx(conn, target_addr, txid, voutid, amount)
+  print partial_tx
+
+  print
+  print "SIGNING RAW TX"
+  # by default, uses all available private keys
+  signed_partial_tx = sign_rawtx(conn, partial_tx)
+  print signed_partial_tx
+
+  print
+  fn = "qr.png"
+  print "GENERATING QR CODE for partial transaction: %s" % fn
+  import qrencode as qr
+  qrcode = qr.encode_scaled(signed_partial_tx, 512)
+  qrcode[2].save(fn, format="png")
 
 if __name__ == "__main__":
-  run()
-  #try:
-  #  run()
-  #except Exception as e:
-  #  print "ERROR"
-  #  print e
-  #  print e.error
+  #run()
+  try:
+    run()
+  except Exception as e:
+    print "ERROR"
+    print e
+    print e.error
